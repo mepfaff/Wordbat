d26f29a77a35154996de049beadc6c941563b53b
Molly Pfaff <molly.e.pfaff@gmail.com>
2017-08-13 15:10:37 -0400

    Fixed bug where repeated words were displayed too many times.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 var txt, highest;
 var repeats = {};
+var displayMax = 100;
 
 // on document ready
 $(function() {
+  
   // new quill editor
   var quill = new Quill("#editor", {
     theme: "snow",
@@ -11,17 +13,15 @@ $(function() {
   }); // end new quill
 
   // set initial contents
-  quill.setText("Write something here...");
+  quill.setText("Write write something here...");
 
   // test button to do
-  $(".navbar-brand").click(function() {
-    // format text and split into a sorted array
+  $(".navbar-brand").click(function(){
+     // format text and split into a sorted array
     txt = quill
       .getText() // gets text from quill editor
-      .toLowerCase()
-      // replaces all that's not letters, numbers, spaces, and apostrophes with a single space
-      .replace(/[^a-z'\s\d]/g, " ")
-      // replaces multiple spaces with a single space
+      .toLowerCase() // replaces all that's not letters, numbers, spaces, and apostrophes with a single space
+      .replace(/[^a-z'\s\d]/g, " ") // replaces multiple spaces with a single space
       // todo: can you change this from "one or more" to "more than one"?
       .replace(/\s+/g, " ")
       .trim() // removes space from beginning and end
@@ -30,17 +30,30 @@ $(function() {
 
     console.log(txt);
     beginRepeatCheck();
-    displayRepeats();
-  }); // end cilck text button
+    return;
+  });
+  
+  $(".test-btn").click(function(){
+    $(".repeats").empty();
+  });
+  
 }); // end document ready function
 
-function beginRepeatCheck() {
+
+var beginRepeatCheck = function() {
+  
+  repeats = {};
+  
+  // display loading
+  $(".repeats").html("Loading...");
+  
   // keep track of the word with the highest count
   highest = 0;
 
   // iterate through every word in the arrray
   var len = txt.length;
   for (var i = 0; i < len; i++) {
+    
     // keep count of the ith word in txt array
     var count = 0;
 
@@ -56,19 +69,19 @@ function beginRepeatCheck() {
     // store repeats in an array within an object with count as key
     if (count > 1) {
       // initialize the array if word is the first with its count
-      if (repeats[count] === undefined) {
+      if (repeats[count] === undefined)
         repeats[count] = [];
-      }
 
-      // store the word in repeats[count]
+      // store the ith word in repeats[count]
       repeats[count].push(txt[i]);
     } // end of count > 1
 
     // move up to the next unique word
     i += count - 1;
   } // end for loop
-
-  return;
+  
+  displayRepeats();
+  
 } // end beginRepeatCheck
 
 // recursively checks sorted array for duplicates, returns duplicate count
@@ -82,27 +95,61 @@ function check(c, count) {
 }
 
 // to do
-function displayRepeats() {
+var displayRepeats = function() {
+  // clear box
+  $(".repeats").empty();
+  
+  // keep track of total displayed
+   var total = 0;
+  
   // iterate from highest to lowest repeat count
   for (var i = highest; i > 1; i--) {
+
     // check to see if that count has words stored in it
     if (repeats.hasOwnProperty(i)) {
       // get each word from the stored array
       var wordArr = repeats[i];
-      var len = wordArr.length;
-      for (var j = 0; j < len; j++) {
-        // display the word and its count
+      
+      // list all words in that array
+      var arrLen = wordArr.length;
+      for (var j = 0; j < arrLen; j++) {
         display(wordArr[j], i);
-      }
-    }
+        
+        // stop displaying words after max
+        total++;
+        if (total > displayMax){         
+          j = arrLen;
+          i = 1;
+        }
+      } // end j for loop
+    } // end if hasownproperty
   } // end i for loop
+  return;
 } // end retrieveRepeats
 
 // display the repeats
 function display(word, count){
+  
   // to do: get from the page how many results to display
   
   // get and check against ignored words and most common words
   
-  console.log(word+": "+count);
+  var newWord = '<div class="checkbox word"><label><input type="checkbox" value="">'+word+': '+count+'</label></div>';
+    
+  // display the word in the list of repeats
+  $(".repeats").append(newWord);
+  
+
 }
+
+// to do: on checking a checkbox next to a repeated word
+// add that word to the list of highlighted words
+// highlight all instances of that word in quill editor
+
+
+// to do: on ignoring a word
+// add it to list of ignored words
+// remove it from the display
+// get and display the next word from the list of repeats
+
+/* note to self: how will highlighting, display, and repeat checks adapt as user makes changes to the text? Which user preferences (such as which words to highlight) will be saved between repeat checks? */
